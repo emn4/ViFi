a1b9e8f32ee449afce264acd428ead8d13446c7e
Edward Ni <eni@login4.ufhpc>
2019-06-21 17:13:09 -0400

    Fixed downloading scripts

---
+++ scripts/accession_parser.py
@@ -0,0 +1,23 @@
+import os
+from Bio import Entrez
+import re
+from io import StringIO
+import numpy as np
+
+accession_path = input(
+    "Input the path to accession list as a .txt file, making sure that each accession is separated by a comma.")
+
+accession_data = np.asarray(np.genfromtxt(accession_path, skiprows = 2))
+
+search_term = input(
+    "Input the host you are searching for")
+
+pattern = re.compile(search_term)
+
+accession_data = accession_data[pattern.search(accession_data[:,2]) != None]
+
+accession_data = accession_data[:,1]
+
+my_list = ','.join(map(accession_data, my_list))
+
+print("Completed")
---
+++ scripts/fasta_download.py
@@ -0,0 +1,18 @@
+import os
+from Bio import Entrez
+import argparse
+
+parser = argparse.ArgumentParser(description='Enter the accession number of the verome')
+
+args = parser.parse_args()
+
+filename = args[0] + ".fasta"
+if not os.path.isfile(filename):
+    net_handle = Entrez.efetch(db="nucleotide", id=args[0], rettype="fasta", retmode="text")
+    out_handle = open(filename, "w")
+    out_handle.write(net_handle.read())
+    out_handle.close()
+    net_handle.close()
+    print("Saved FASTA of " + args[0])
+else:
+    print("Error: FASTA already exists")
